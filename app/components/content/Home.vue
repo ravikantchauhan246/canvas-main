965bd6e572c07c42be873f8fd624bcaddc157282
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2025-02-01 14:43:50 +0530

    Remove Home page components and delete FAQ component

--- app/components/content/Home.vue
+++ app/components/content/Home.vue
@@ -60,15 +60,12 @@
           class="mt-12 flex w-full max-w-3xl flex-col gap-4 lg:mt-16"
         >
           <!-- projects -->
-          <HomeProjects />
+          
         </div>
 
-        <Divider class="my-9" />
+        
 
-        <HomeFaq
-          style="--stagger: 6"
-          data-animate
-        />
+        
       </div>
     </div>
   </section>
